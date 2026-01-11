141f34216b256873c6082dedbdf2dd360307712d
Ashutosh Kumar <ashutoshpw@users.noreply.github.com>
2026-01-11 17:37:48 +0530

    added astro

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,19 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use('/*', cors())
 
 const welcomeStrings = [
   'Hello Hono!',
   'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
 ]
 
-app.get('/', (c) => {
+const routes = app.get('/', (c) => {
   return c.text(welcomeStrings.join('\n\n'))
 })
+
+export type AppType = typeof routes
 
 export default app
